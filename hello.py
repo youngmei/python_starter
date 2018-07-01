dc950238e52ae722ddc5b1fe6dffed1d75fd86b9
unknown <youngmeizhang@163.com>
2018-07-01 21:42:52 +0800

    hello world

---
+++ hello.py
@@ -0,0 +1,10 @@
+#This is my first python programe. I begin to learn Python on Jul.1 2018. 
+#dfqervn dfje feronclveqrogn v dovnre cxoefna  eofnkcv eofneof efbrvo vcnkl lf elldaln alfe qefb alfbql lfbql 
+print ("Hello Josen!")
+print ('Where are you from?')
+h_bar=1.05457e-34;
+print ("h_bar=")
+print( h_bar)
+pi=3.1415926
+h=2*pi*h_bar
+print(h)
